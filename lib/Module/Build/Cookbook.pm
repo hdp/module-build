d732e8fa4765c05eda182de502cf34a91673fab4
David Golden <dagolden@cpan.org>
2009-09-17 23:02:28 +0000

    Bump version to 0.3500_01 for maintenance series
    git-svn-id: http://svn.perl.org/modules/Module-Build/branches/0.3500@13360 50811bd7-b8ce-0310-adc1-d9db26280581

--- lib/Module/Build/Cookbook.pm
+++ lib/Module/Build/Cookbook.pm
@@ -1,7 +1,7 @@
 package Module::Build::Cookbook;
 use strict;
 use vars qw($VERSION);
-$VERSION = '0.35';
+$VERSION = '0.3500_01';
 
 
 =head1 NAME
--- lib/Module/Build/ModuleInfo.pm
+++ lib/Module/Build/ModuleInfo.pm
@@ -8,7 +8,7 @@ package Module::Build::ModuleInfo;
 
 use strict;
 use vars qw($VERSION);
-$VERSION = '0.35';
+$VERSION = '0.3500_01';
 $VERSION = eval $VERSION;
 
 use File::Spec;
